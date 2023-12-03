5f0f6ae0fdb6421a91eb5a4d145d9c726276e27b
Rector Ratsaka <rectorratsaka3@gmail.com>
2023-12-03 04:45:00 +0200

    backgroud color for active mode

--- ScientificCalculator.js
+++ ScientificCalculator.js
@@ -3,37 +3,61 @@
 let expression = '';
 let outputElement = document.getElementById('OutPut');
 let inputElement = document.getElementById('InPut');
-let trigMode = 'rad'; // Default mode is radians
 
-function toggleDarkLightMode() {
-            var body = document.body;
-            body.classList.toggle("dark-mode");
+function toggleDarkLightMode(mode) {
+    var body = document.body;
+    if (mode === 'light') {
+        body.classList.remove("dark-mode");
+        body.classList.add("light-mode");
+        document.getElementById("lightM").classList.add("blue-button");
+        document.getElementById("darkM").classList.remove("blue-button");
+    } else if (mode === 'dark') {
+        body.classList.remove("light-mode");
+        body.classList.add("dark-mode");
+        document.getElementById("darkM").classList.add("blue-button");
+        document.getElementById("lightM").classList.remove("blue-button");
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+        handleKeyPress(event.key);
+    });
+
+    function handleKeyPress(key) {
+        // Check if the pressed key is a number or an operator
+        if (!isNaN(key) || "+-*/.".includes(key)) {
+            r(key); // Call the function to handle the input
+        } else if (key === "Enter") {
+            r('='); // Pressing Enter will act as "="
+        } else if (key === "Backspace") {
+            r('back'); // Pressing Backspace will act as backspace
         }
+    }
 
 function r(value) {
    if (value === '=') {
-       calculate(); // Trigger calculation
-     } else if (value === 'C') {
-       expression = ''; // Clear the entire expression
-       outputElement.innerText = 0;
-     } else if (value === 'back') {
-       expression = expression.slice(0, -1); // Remove the last character
-     } else {
-       const funcs = ['sin', 'cos', 'tan', 'sqrt','exp','log10'];
-       if (funcs.includes(value)) {
-         expression += `Math.${value}(`;
-       } else if (value === '%') {
-         expression += '/100';
-       } else if (value === 'pi') {
-         expression += 'Math.PI';
-       }else if (value=='fact'){
-         expression+='factorial(';
-       }else if (value=='pow'){
-         expression+='**';
-       } else {
-         expression += value;
-       }
-     }
+    calculate(); // Trigger calculation
+ } else if (value === 'C') {
+   expression = ''; // Clear the entire expression
+   outputElement.innerText = 0;
+ } else if (value === 'back') {
+   expression = expression.slice(0, -1); // Remove the last character
+ } else {
+   const funcs = ['sin', 'cos', 'tan', 'sqrt','exp','log10'];
+   if (funcs.includes(value)) {
+     expression += `math.${value}(`;
+   } else if (value === '%') {
+     expression += '/100';
+   } else if (value === 'pi') {
+     expression += 'math.pi';
+   }else if (value=='fact'){
+     expression+='factorial(';
+   }else if (value=='pow'){
+     expression+='**';
+   } else {
+     expression += value;
+   }
+ }
   updateDisplay();
 }
 
@@ -41,9 +65,15 @@ function updateDisplay() {
   inputElement.innerText = expression;
 }
 
+function isValidInput(expression) {
+  const validInputRegex = /^[0-9+\-*/().\s]+$/;
+  return validInputRegex.test(expression);
+}
+
+
 function calculate() {
   try {
-    const result = eval(expression);
+    const result = math.evaluate(expression);
     outputElement.innerText = result.toString();
     updateDisplay();
   } catch (error) {
@@ -51,25 +81,6 @@ function calculate() {
     updateDisplay();
   }
 }
-
-//function setTrigMode(mode) {
-//  trigMode = mode;
-//  // Remove 'active' class from all buttons
-//  document.getElementById('degButton').classList.remove('active');
-//  document.getElementById('radButton').classList.remove('active');
-//  // Add 'active' class to the selected button
-//  document.getElementById(`${mode}Button`).classList.add('active');
-//
-//  updateDisplay();
-//}
-//
-//
-//// trigMode will be 'deg' or 'rad'
-//function trigFunctions(value) {
-//  if (trigMode === 'deg') {
-//    // Convert degrees to radians before calling trigonometric functions
-//    value = (value * Math.PI) / 180;
-//  }
 
 function factorial(n){
     if (n==0 || n==1){
